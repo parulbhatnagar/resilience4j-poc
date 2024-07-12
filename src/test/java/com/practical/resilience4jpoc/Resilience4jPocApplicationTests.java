d4131a1a98c66daa09e49e6b27d87f2e0c1e22a4
ParulBhatnagar <parulbhatnagar@gmail.com>
2024-07-12 21:11:16 +0530

    Initial commit

---
+++ src/test/java/com/practical/resilience4jpoc/Resilience4jPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practical.resilience4jpoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Resilience4jPocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
